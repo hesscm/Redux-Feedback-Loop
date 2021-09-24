591d932b89dc8a28bb009ebf554cd7d0982edf12
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-24 16:25:15 -0500

    setup a redux store in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//reducer to temporarily store survery answers
+const surveyData = (state = {}, action) => {
+    //in surveyData reducer
+    return state;
+}
+
+//create redux instance to hold all reducers in a single store
+const storeInstance = createStore(
+    combineReducers({
+        surveyData
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    //wrap App in a provider to make Redux available everywhere
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
